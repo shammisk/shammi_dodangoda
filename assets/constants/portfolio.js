9444f4dc6317c5720c792e8e770c2a65ec848498
shammisk <ssamankumari6@gmail.com>
2025-07-21 17:11:10 +0530

    add assest

---
+++ assets/constants/portfolio.js
@@ -0,0 +1,44 @@
+const portfolioItems = [
+  {
+    id: "1",
+    category: "web",
+    title: "LEO club Website",
+    description: "Explore the vibrant Leo Club of District 306 C2 at the University of Sri Jayawardenepura.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "2",
+    category: "web",
+    title: "App 2",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "3",
+    category: "desktop",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "4",
+    category: "iot",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "5",
+    category: "hrm",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "6",
+    category: "iot",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+];
