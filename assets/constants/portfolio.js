999d3356accfb715a3c94423ee7b9873ea1e86a9
shammisk <ssamankumari6@gmail.com>
2025-07-21 20:39:48 +0530

    portfolio edits

--- assets/constants/portfolio.js
+++ assets/constants/portfolio.js
@@ -3,15 +3,23 @@ const portfolioItems = [
     id: "1",
     category: "web",
     title: "LEO club Website",
-    description: "Explore the vibrant Leo Club of District 306 C2 at the University of Sri Jayawardenepura.",
-    image: "assets/img/portfolio/books-1.jpg",
+    description:
+      "Explore the vibrant Leo Club of District 306 C2 at the University of Sri Jayawardenepura.",
+    image: "assets/img/portfolio/Leoweb.png",
   },
   {
     id: "2",
     category: "web",
-    title: "App 2",
+    title: "CTG Motors Website",
     description: "A slick website design.",
-    image: "assets/img/portfolio/books-1.jpg",
+    image: "assets/img/portfolio/CTG.png",
+  },
+  {
+    id: "2",
+    category: "web",
+    title: "MB Labels Website",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/mb.png",
   },
   {
     id: "3",
