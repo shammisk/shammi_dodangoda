e37fad103172ddfeca1545333ed6972e1d267d6a
shammisk <ssamankumari6@gmail.com>
2025-07-26 22:59:52 +0530

    add new web projects

--- assets/constants/portfolio.js
+++ assets/constants/portfolio.js
@@ -11,40 +11,52 @@ const portfolioItems = [
     id: "2",
     category: "web",
     title: "CTG Motors Website",
-    description: "A slick website design.",
+    description:
+      "Developed a responsive and user-friendly WordPress website for CTG Motors, an Australian automotive business. The site highlights services, vehicle listings, and contact options with a clean design and mobile optimization.",
     image: "assets/img/portfolio/CTG.png",
   },
   {
-    id: "2",
+    id: "3",
     category: "web",
     title: "MB Labels Website",
-    description: "A slick website design.",
+    description:
+      "Improved the MB Labels online store by integrating a Retail POS system and implementing SEO strategies tailored for Brisbane, Australia. Managed product categorization, URL structure, and keyword optimization using WordPress and WooCommerce to enhance user experience and search engine visibility.",
     image: "assets/img/portfolio/mb.png",
   },
   {
-    id: "3",
-    category: "desktop",
-    title: "Web Project 1",
-    description: "A slick website design.",
-    image: "assets/img/portfolio/books-1.jpg",
-  },
-  {
     id: "4",
-    category: "iot",
-    title: "Web Project 1",
-    description: "A slick website design.",
-    image: "assets/img/portfolio/books-1.jpg",
+    category: "web",
+    title: "Reef Villa – Rates & Bookings Update (2025)",
+    description:
+      "Updated and optimized the Rates & Bookings section of a client’s accommodation website by revising pricing details, aligning content with the site's tone, and enhancing clarity for users. Ensured mobile responsiveness and cross-browser compatibility for a smooth booking experience.",
+    image: "assets/img/portfolio/reefvilla.png",
   },
   {
     id: "5",
+    category: "web",
+    title: "Villa Frangipani – Website Maintenance (2024)",
+    description:
+      "Performed key updates on the Villa Frangipani website, including updating the “Book Now” link and resetting the admin password for improved security and functionality.",
+    image: "assets/img/portfolio/Villa.png",
+  },
+  {
+    id: "6",
     category: "hrm",
     title: "HRM Research",
     description: "A slick website design.",
     image: "assets/img/portfolio/DSI.png",
   },
   {
-    id: "6",
+    id: "7",
     category: "iot",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+
+  {
+    id: "8",
+    category: "desktop",
     title: "Web Project 1",
     description: "A slick website design.",
     image: "assets/img/portfolio/books-1.jpg",
