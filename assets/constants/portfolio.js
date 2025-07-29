a0e005e77af4fdfa0e18f8c651d35a5fe15166cb
shammisk <ssamankumari6@gmail.com>
2025-07-29 08:50:43 +0530

    updare project links

--- assets/constants/portfolio.js
+++ assets/constants/portfolio.js
@@ -6,8 +6,8 @@ const portfolioItems = [
     description:
       " Experience the essence of service and leadership with the Leo Club of District 306 C2 at the University of Sri Jayawardenepura, showcasing our impactful projects, prestigious awards, inspiring anecdotes, dedicated board members, and contact details, encapsulating our commitment to community and personal development. (2024)",
     image: "assets/img/portfolio/Leoweb.png",
-    live: "https://your-live-link.com", 
-    github: "https://github.com/your-repo", 
+    live: "https://japuraleo.github.io/org.lk/",
+    github: "https://github.com/Japuraleo/org.lk",
   },
   {
     id: "2",
@@ -16,6 +16,7 @@ const portfolioItems = [
     description:
       "Developed a responsive and user-friendly WordPress website for CTG Motors, an Australian automotive business. The site highlights services, vehicle listings, and contact options with a clean design and mobile optimization.",
     image: "assets/img/portfolio/CTG.png",
+    live: "https://ctgmotors.com.au/",
   },
   {
     id: "3",
@@ -24,6 +25,7 @@ const portfolioItems = [
     description:
       "Improved the MB Labels online store by integrating a Retail POS system and implementing SEO strategies tailored for Brisbane, Australia. Managed product categorization, URL structure, and keyword optimization using WordPress and WooCommerce to enhance user experience and search engine visibility.",
     image: "assets/img/portfolio/mb.png",
+    live: "https://mblabels.com.au/",
   },
   {
     id: "4",
@@ -32,6 +34,7 @@ const portfolioItems = [
     description:
       "Updated and optimized the Rates & Bookings section of a client’s accommodation website by revising pricing details, aligning content with the site's tone, and enhancing clarity for users. Ensured mobile responsiveness and cross-browser compatibility for a smooth booking experience.",
     image: "assets/img/portfolio/reefvilla.png",
+    live: "https://www.reefvillaportdouglas.com.au/index.php/rates-bookings",
   },
   {
     id: "5",
@@ -40,6 +43,7 @@ const portfolioItems = [
     description:
       "Performed key updates on the Villa Frangipani website, including updating the “Book Now” link and resetting the admin password for improved security and functionality.",
     image: "assets/img/portfolio/villa.png",
+    live: "https://villafrangipaniportdouglas.com.au/",
   },
   {
     id: "6",
@@ -48,6 +52,7 @@ const portfolioItems = [
     description:
       "Completed a practical project on Training and Development at DSI (Pvt) Ltd., including a visit to the Bataduwa branch and discussions with the HR Manager. Gained valuable insights into the company’s employee development strategies and HR practices.",
     image: "assets/img/portfolio/DSI.png",
+    live: "https://drive.google.com/file/d/1WF9d8QxQzB3naQI4vgF1r0xfDOEK_ZaJ/view",
   },
   {
     id: "7",
@@ -56,6 +61,8 @@ const portfolioItems = [
     description:
       "Developed an advanced IoT-based home security prototype featuring light and water motor control, fire detection with alarms, wall and door security sensors, and gate access with password protection. Includes real-time monitoring and remote access, offering a secure and smart solution for modern home management.",
     image: "assets/img/portfolio/smarthome.png",
+    live: "",
+    github: "https://github.com/shammisk/IoT_based_Home_Security_System",
   },
 
   {
@@ -65,6 +72,8 @@ const portfolioItems = [
     description:
       "Developed a C#-based Student Registration System as the final project for the DiTEC course at ESOFT Metro Campus. The project enhanced my skills in C# programming and SQL database management, focusing on effective handling of student information.",
     image: "assets/img/portfolio/stu.png",
+
+    github: "https://github.com/shammisk/Esoft_Final_Project",
   },
   {
     id: "9",
@@ -80,6 +89,7 @@ const portfolioItems = [
     description:
       "Motivated by the Covid-19 situation, I developed a secure and user-friendly system that allows customers to enter and save their details upon entry. Designed for shop owners to retrieve customer data by date, the system supports accurate record-keeping, access control, and data privacy compliance, improving safety and decision-making during health-sensitive periods.",
     image: "assets/img/portfolio/custom.png",
+    github: "https://github.com/shammisk/Customer_Data_Collecting_System",
   },
   {
     id: "11",
@@ -88,6 +98,7 @@ const portfolioItems = [
     description:
       "Developed a user-friendly desktop application for managing sales transactions in a sweet shop. The system enables automatic total calculations, bill generation, and offers options to print or email bills, streamlining the checkout process and improving customer service.",
     image: "assets/img/portfolio/bil.png",
+    github: "https://github.com/shammisk/Billing_System",
   },
   {
     id: "12",
@@ -96,5 +107,6 @@ const portfolioItems = [
     description:
       "Created a desktop application to calculate Body Mass Index (BMI) using user input for height and weight. The tool provides instant results and classifies the BMI into categories such as underweight, normal, overweight, or obese, supporting quick health assessments.",
     image: "assets/img/portfolio/bmi.png",
+    github: "https://github.com/shammisk/BMI-Calculator",
   },
 ];
